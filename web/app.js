0aaad3a506a166eaea407633cd4c2a1554355e47
Rakesh Das <rakeshdaskowshik@gmail.com>
2016-12-16 12:27:55 -0600

    Testing JSON from url

--- web/app.js
+++ web/app.js
@@ -1,17 +1,31 @@
 var express = require('express');
-// var routes = require('./routes/index');
+var request = require('request');
 var cookieParser = require('cookie-parser');
 
 
 
 var app = express();
 
+var jsonURL = "http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=838ed1580f4242f09a361dfb6b8ff32c&rt=red&outputType=json";
+
+var trains;
+
 var tagline = "this is a tagline and it is pretty pointless!"
   //set view engine to ejs
 app.set('view engine', 'ejs');
 
-//res.render loads ejs files
+//request json
+request({
+  url:jsonURL,
+  json: true
+},function(err, res, body){
+  if(!err && res.statusCode === 200){
+    trains = body.ctatt.route[0].train;
+    console.log(trains);
+  }
+})
 
+//res.render loads ejs files
 app.get('/', function(req, res) {
   var sampleJSON = [{
     name: 'John Oliver',
@@ -26,12 +40,12 @@ app.get('/', function(req, res) {
 
   res.render('routes/index', {
     nightHosts: sampleJSON,
-    tagline: tagline
+    tagline: tagline,
+    trains: trains
   });
 });
 
 //about pg
-
 app.get('/about', function(req, res) {
   res.render('routes/about');
 });
