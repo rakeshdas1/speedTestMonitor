cb976279d6960746df55ae08e3cfd250a5c95710
Rakesh Das <rakeshdaskowshik@gmail.com>
2016-12-15 23:55:16 -0600

    Messing around with node and ejs and express

--- web/app.js
+++ web/app.js
@@ -1,30 +1,40 @@
 var express = require('express');
-var routes = require('./routes/index');
+// var routes = require('./routes/index');
 var cookieParser = require('cookie-parser');
+
 
 
 var app = express();
 
-app.set('views', __dirname+'/views');
+var tagline = "this is a tagline and it is pretty pointless!"
+  //set view engine to ejs
 app.set('view engine', 'ejs');
 
-app.use('/', routes);
+//res.render loads ejs files
 
+app.get('/', function(req, res) {
+  var sampleJSON = [{
+    name: 'John Oliver',
+    age: 41
+  }, {
+    name: 'Jimmy Fallon',
+    age: 43
+  }, {
+    name: 'Jimmy Kimmel',
+    age: 37
+  }];
 
-
-app.use(function(res, req, next){
-  var err = new Error('404 Not Found!');
-  err.status = 404;
-  next(err);
-})
-
-
-app.use(function(err, req, res, next){
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: err
+  res.render('routes/index', {
+    nightHosts: sampleJSON,
+    tagline: tagline
   });
 });
+
+//about pg
+
+app.get('/about', function(req, res) {
+  res.render('routes/about');
+});
+
 
 app.listen(8888);
